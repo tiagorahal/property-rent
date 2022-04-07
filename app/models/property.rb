dad0c700cf53e3ef59b0a42eb1a3b702eeb81e9c
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-04-07 03:04:52 -0300

    Fix linter issues

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,5 +1,5 @@
 class Property < ApplicationRecord
   has_many_attached :pictures
   validates :name, presence: true
-  validates :pictures, presence: true, :length => { :minimum => 3, :maximum => 5}
+  validates :pictures, presence: true, length: { minimum: 3, maximum: 5 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+50.times do |i|
+  Property.create!(name: "Testing Name", address:  "Testing Address", rooms: 3, bathrooms: 2, price: 100000, pictures: [] )
+end
